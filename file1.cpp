c738323185e67c2870548d5ba6710168e91fb388
hermayani2786 <herma.yani2786@gmail.com>
2026-03-10 20:24:12 +0700

    menentukan status BMI

--- file1.cpp
+++ file1.cpp
@@ -13,3 +13,15 @@ void inputData(float &berat, float &tinggi){
 float hitungBMI(float berat, float tinggi){
     return berat / (tinggi * tinggi);
 }
+
+// Fungsi Menentukan status BMI
+string statusBMI(float bmi){
+    if (bmi < 18.5)
+        return "Berat badan kurang";
+    else if( bmi < 25)
+        return "Berat badan normal";
+    else if (bmi < 30)
+        return "Berat badan kelebihan";
+    else
+        return "Obesistas";
+}
